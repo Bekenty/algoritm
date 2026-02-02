4793e8116300ee8689241ca982a0a9211f26a6bb
Bekenty <bekenty.sven@gmail.com>
2026-02-02 15:05:32 +0500

    bek

---
+++ lab 1 bek/3 question.py
@@ -0,0 +1,9 @@
+# Циклический алгоритм
+
+n = 125
+
+total = 0
+for i in range(1, n + 1):
+    total += i
+
+print(total)
---
+++ lab 3 bek/5 question.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+if a >= b:
+    if a > b:
+        print("Первое число больше")
+    else:
+        print("Числа равны")
+else:
+    print("Второе число больше")
